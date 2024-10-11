fe724cea5f407bcb5f6843031bc6556e4edb90c6
djarvis31 <143646404+djarvis31@users.noreply.github.com>
2024-10-11 13:04:05 -0400

    Update LED.h

--- LED.h
+++ LED.h
@@ -1,8 +1,16 @@
-#ifndef LED
-#define LED
+#ifndef LED_h
+#define LED_h
 
-#include<Arduino.h>
+#include <Arduino.h>
 
-void test_func();
+class LED {
+  public:
+    LED(int pin);   // Constructor to initialize the LED pin
+    void turnOn();  // Function to turn on the LED
+    void turnOff(); // Function to turn off the LED
+    void blink(int delayTime); // Function to blink the LED with a delay
+  private:
+    int _pin;       // Pin associated with the LED
+};
 
 #endif
