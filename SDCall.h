2bc21db98213f152c7e028d93347a629bfcb12c4
djarvis31 <143646404+djarvis31@users.noreply.github.com>
2024-10-11 12:38:01 -0400

    Update SDCall.h

--- SDCall.h
+++ SDCall.h
@@ -1,8 +1,12 @@
 #ifndef SDCall
 #define SDCall
 
-#include<Arduino.h>
+#include <Arduino.h>
+#include <FS.h>
+#include <SD.h>
+#include <SPI.h>
 
+// Define a test function for SD card calls
 void test_func();
 
 #endif
