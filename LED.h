963421056514655a35156aa9d654c1665639a3cc
djarvis31 <143646404+djarvis31@users.noreply.github.com>
2024-10-25 09:12:08 -0400

    Update LED.h

--- LED.h
+++ LED.h
@@ -1,8 +1,31 @@
-#ifndef LED
-#define LED
+#include "led.h"
 
-#include<Arduino.h>
+// Constructor: Initializes the LED pin
+LED::LED(int pin) {
+  ledPin = pin;
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);  // Ensure LED is off by default
+}
 
-void test_func();
+// Method to turn the LED on
+void LED::turnOn() {
+  digitalWrite(ledPin, HIGH);
+}
 
-#endif
+// Method to turn the LED off
+void LED::turnOff() {
+  digitalWrite(ledPin, LOW);
+}
+
+// Method to toggle the LED (turn on if off, or off if on)
+void LED::toggle() {
+  int state = digitalRead(ledPin);
+  digitalWrite(ledPin, !state);
+}
+
+// Method to blink the LED for a given duration
+void LED::blink(int duration) {
+  turnOn();
+  delay(duration);
+  turnOff();
+}
