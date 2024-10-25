515563db19916b7e3b6e4e3c9875cff88af3f291
djarvis31 <143646404+djarvis31@users.noreply.github.com>
2024-10-25 09:09:20 -0400

    Update Button.h

--- Button.h
+++ Button.h
@@ -1,9 +1,11 @@
-#ifndef BUTTONH
-#define BUTTONH
-
-#include<Arduino.h>
-
-void test_func();
-
-#endif
-
+void checkButtons() {
+  if (!isPlaying) {
+    if (digitalRead(button1Pin) == LOW) {
+      playWavFile("/ledone.wav", ledone);
+    } else if (digitalRead(button2Pin) == LOW) {
+      playWavFile("/ledtwo.wav", ledtwo);
+    } else if (digitalRead(button3Pin) == LOW) {
+      playWavFile("/ledthree.wav", ledthree);
+    }
+  }
+}
